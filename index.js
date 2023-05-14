d44b35a24514b1d9668d1a36693f62d3bd8ea266
Austin Morse <amorse110@gmail.com>
2023-05-13 22:49:54 -0700

    appends to document

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ main.remove();
 const newHeader = document.createElement("h1");
 newHeader.setAttribute("id","victory");
 newHeader.textContent = "Austin is the champion";
+document.body.append(newHeader);
